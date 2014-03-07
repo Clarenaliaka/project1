dd996b7d28c189c3f24531338cdfc8ee428f078d
Clarenaliaka <yalekky@gmail.com>
2014-03-07 10:24:04 -0500

    Update loops.php

--- loops.php
+++ loops.php
@@ -1,10 +1,18 @@
 <?php
 //This is a for loop
 
-for($i=0; i<=30; i++){
+for($i=0; $i<=30; $i++){
 echo $i;
 
 }
+// Do While loop
+
+$c=20;
+d($c<=100;){
+  $c++;
+  echo $c."<br>"
+}
+while{
 
 
 
